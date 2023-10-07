f2f217fb6585751d6b146f0634d1dfe8da260413
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2023-10-07 01:39:17 +0100

    Battery low

---
+++ 23. Voice Converter/main.js
@@ -0,0 +1,18 @@
+const Btn = document.querySelector("button");
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+
+Btn.addEventListener("click", () =>{
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+})
